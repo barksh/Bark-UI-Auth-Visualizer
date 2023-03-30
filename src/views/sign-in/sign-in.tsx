409b7b3ff79cb263bd6ebcb03fa1b7ce2a4047e9
wmxpy <wm@mengw.io>
2023-03-30 01:10:56 -0500

    add redirect action

--- src/views/sign-in/sign-in.tsx
+++ src/views/sign-in/sign-in.tsx
@@ -5,7 +5,7 @@
  */
 
 import { Button, ButtonGroup, Card, InputText, LoadingContainerRectangle } from "@barksh/bark-design-react";
-import { BarkPopupWindowModel } from "@barksh/client-authentication-browser";
+import { BarkPopupWindowModel, BarkRedirectModel } from "@barksh/client-authentication-browser";
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
 import { barkClient } from "../../util/bark-client";
@@ -18,7 +18,7 @@ export const SignInView: React.FC = () => {
     const [loading, setLoading] = React.useState(false);
     const [domain, setDomain] = React.useState('');
 
-    const signInAction = async () => {
+    const popUpAction = async () => {
 
         const model: BarkPopupWindowModel = barkClient.createPopupWindowModel(
             domain,
@@ -29,6 +29,23 @@ export const SignInView: React.FC = () => {
             const authenticationToken = await model.performInquiry();
             console.log(authenticationToken);
             navigate('/visualizer');
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+            return;
+        }
+    };
+
+    const redirectAction = async () => {
+
+        const model: BarkRedirectModel = barkClient.createRedirectModel(
+            'expose-key',
+            domain,
+        );
+
+        try {
+
+            await model.performInquiry();
         } catch (error) {
             console.log(error);
             setLoading(false);
@@ -48,13 +65,16 @@ export const SignInView: React.FC = () => {
                     <Button
                         onClick={() => {
                             setLoading(true);
-                            signInAction();
+                            popUpAction();
                         }}
                     >
                         Pop Up
                     </Button>
                     <Button
-                        disabled
+                        onClick={() => {
+                            setLoading(true);
+                            redirectAction();
+                        }}
                     >
                         Redirection
                     </Button>
